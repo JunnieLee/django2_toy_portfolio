5b11c3c88d35c54be7e3c15de7ee1169bd11ee03
JunnieLee <heunghari@naver.com>
2018-04-12 11:37:14 +0900

    applied some tips from the tutorial

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,16 @@ class Blog(models.Model):
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
 
+	def __str__(self):
+		return self.title # django admin에서의 각 object display방식을 title로 나타내게 설정하기!
+
+	def summary(self):
+		return self.body[:100] 
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y') # customizing the date details	
+
+
 # Add the Blog app to the settings
 
 # Create a migration
